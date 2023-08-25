d49e699dfa9b773f34e5312647f329b15479c7eb
nastyaclifford <madchen95@mail.ru>
2023-08-24 19:58:28 -0600

    responsive design and sticky menu, routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,41 @@
-import Home from "./Home/Home";
+import Table from "./Table/Table"
+import Slider from "./Slider/Slider"
+import Home from "./Home/Home"
+import "../styles/App.scss" 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRightToBracket, faHandSparkles } from '@fortawesome/free-solid-svg-icons'; //импортируем нужные нам иконки
+import {
+    BrowserRouter, Routes,Link, Route
+} from 'react-router-dom';//импортируем компоненты и стили
 
 function App() {
   return (
-  <div>
-
-    <Home/>
-    </div>)
+    <div className="container">
+    <BrowserRouter>
+    <div className="header">
+                < div className= "logo">
+                    <FontAwesomeIcon icon={faHandSparkles} />
+                </div>
+                <nav className="nav">
+                    <Link className="item" href=" " to="about">About</Link> 
+                    <Link className="item" href=" " to="startpractising">Start Practising</Link> 
+                    <Link className="item" href=" " to="allwords">All Words</Link> 
+                    <Link className="item" href=" " to="additional">Additional</Link>
+                </nav>
+    <div className="login">
+        <div className="button"><FontAwesomeIcon icon={faRightToBracket} /> </div>
+            </div>
+            </div>
+       
+      <Routes>
+    <Route path="startpractising" element={<Slider/>}/>
+    <Route path="allwords" element={<Table/>}/>
+    <Route path="about" element={<Home/>}/>
+       </Routes>
+       </ BrowserRouter>
+    
+    
+            </div>)
 }
 
 export default App;
