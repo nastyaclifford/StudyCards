a49c5f681ed8b364e4f3ef740b328889e11f2665
nastyaclifford <anastasiaclifford95@gmail.com>
2023-11-15 12:52:02 -0600

    fixing requests

--- src/services/GET.js
+++ src/services/GET.js
@@ -1,7 +1,7 @@
 export class GET {
   static async getWord() {
     try {
-      const resp = await fetch("http://itgirlschool.justmakeit.ru/api/words");
+      const resp = await fetch("https://itgirlschool.justmakeit.ru/api/words");
       const data = await resp.json();
       return data;
     } catch (e) {
